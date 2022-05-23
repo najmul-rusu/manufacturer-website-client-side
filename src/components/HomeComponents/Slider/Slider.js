7d76d5941b216e0f9982852542a730baf3e35367
najmul-rusu <nhrusu@gmail.com>
2022-05-23 13:57:31 +0600

    slider banner modify

--- src/components/HomeComponents/Slider/Slider.js
+++ src/components/HomeComponents/Slider/Slider.js
@@ -6,25 +6,21 @@ const Slider = () => {
   return (
     <div className="carousel w-full">
       <div id="slide1" className="carousel-item relative w-full">
-      <figure><img src={Slider1} className="w-full" alt="Slider1" /></figure>
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide4" className="btn btn-circle">
-            ❮
-          </a>
-          <a href="#slide2" className="btn btn-circle">
-            ❯
-          </a>
-        </div>
-      </div>
-      <div id="slide2" className="carousel-item relative w-full">
-      <figure><img src={Slider2} className="w-full" alt="Slider2" /></figure>
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide1" className="btn btn-circle">
-            ❮
-          </a>
-          <a href="#slide3" className="btn btn-circle">
-            ❯
-          </a>
+        <figure>
+          <img src={Slider1} className="w-full" alt="Slider1" />
+        </figure>
+        <div className="absolute transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <div>
+            <h3 className="text-left text-4xl text-white font-bold">
+              NEW TECHNOLOGY & BUILD
+            </h3>
+            <h2 className="text-left text-7xl text-white font-bold">
+              WHEELS & TIRES
+            </h2>
+            <h2 className="text-left text-7xl text-white font-bold">
+              COLLECTIONS
+            </h2>
+          </div>
         </div>
       </div>
     </div>
