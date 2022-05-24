29e91f20c8f008729f6c9704c59bab3f8b3ad575
najmul-rusu <nhrusu@gmail.com>
2022-05-24 17:16:05 +0600

    some products added in home page

--- src/components/HomeComponents/HomeProducts.js
+++ src/components/HomeComponents/HomeProducts.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import ProductsData from '../../hook/ProductsData';
+import ShowProducts from '../Products/ShowProducts';
 import SectionTitle from '../Title/SectionTitle';
 
 const HomeProducts = () => {
+    
+    const [products] = ProductsData();
     return (
-        <div>
-            <SectionTitle>Parts</SectionTitle>
+      <div className="px-12">
+          <SectionTitle>Parts</SectionTitle>
+        <div className="grid grid-cols-3 gap-10">
+            {products.slice(0, 6).map((product) => (
+              <ShowProducts
+                product={product}
+                key={product._id}
+              ></ShowProducts>
+            ))}
         </div>
+      </div>
     );
-};
+  };
 
 export default HomeProducts;
--- src/components/Products/ShowProducts.js
+++ src/components/Products/ShowProducts.js
@@ -5,25 +5,25 @@ const ShowProducts = ({ product }) => {
   const { _id, picture, name, description, price, quantity, minimum_quantity } =
     product;
   const navigate = useNavigate();
-  const handlenavigateToUpdateStock = (id) => {
+  const handlenavigateToPurchase = (id) => {
     navigate(`/product/${id}`);
   };
 
   return (
-    <div class="card card-compact w-96 bg-base-100 shadow-xl">
+    <div className="card card-compact w-96 bg-base-100 shadow-xl">
       <figure>
         <img className="" src={picture} alt="iPhone" />
       </figure>
-      <div class="card-body">
-        <h2 class="card-title">{name}</h2>
+      <div className="card-body">
+        <h2 className="card-title">{name}</h2>
         <p className="card-text">Quantity: {quantity}</p>
         <p className="card-text text-muted">
           Minimum Order Quantity: {minimum_quantity}
         </p>
         <p className="card-text">Price: ${price}</p>
         <p className="card-text pb-4">{description}</p>
-        <div class="card-actions justify-end">
-          <button className="btn btn-primary w-100" onClick={()=>handlenavigateToUpdateStock(_id)}>Buy Now</button>
+        <div className="card-actions justify-end">
+          <button className="btn btn-primary w-100" onClick={()=>handlenavigateToPurchase(_id)}>Buy Now</button>
         </div>
       </div>
     </div>
